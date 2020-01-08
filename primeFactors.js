8ddeca1ec92f2d0e93f46eed0c9527e2318484d5
Antonio Rios <antonio.rios.jr@gmail.com>
2020-01-07 21:05:50 -0700

    Updated the index.html file with a immediate invoke function express to prevent polluting the global scope. Added an implementation of primeFactors function.

---
+++ primeFactors.js
@@ -0,0 +1,28 @@
+/**
+ * primeFactors.js
+ *
+ */
+function primeFactors(n){
+	//print the number of 2s that dive n
+	while (n % 2 == 0) {
+		console.log(2);
+		n = n / 2;
+	}
+
+    // n must be odd at this point. so we can skip one element
+    // (Note i = i + 2)
+    for(var i = 3; i * i <= n; i = i + 2) {
+        // while i divides n, print i and divide n
+        while(n % i == 0){
+            console.log(i);
+            n = n / i;
+        }
+    }
+
+    // this condition is to handle the case when n is a prime number
+    // greater than 2
+    if(n > 2){
+        console.log(n);
+    }
+}
+
